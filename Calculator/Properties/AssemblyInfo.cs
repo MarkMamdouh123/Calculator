01d0c3a6b7120f6910b30f5a341bcf674ccf833c
Mark Mamdouh <mark.mamdouh67@yahoo.com>
2023-08-16 00:17:31 +0300

    ultered properties

--- Calculator/Properties/AssemblyInfo.cs
+++ Calculator/Properties/AssemblyInfo.cs
@@ -8,10 +8,10 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Calculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Mark")]
 [assembly: AssemblyProduct("Calculator")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Mark Mamdouh")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
